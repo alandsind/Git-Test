568de0c7869af938bfffebef9d7d086ce83838ea
Aland Sinduartha <alandsind@gmail.com>
2019-05-15 08:04:14 +0700

    Initial Commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Aland Sinduartha on 15/05/19.
+//  Copyright © 2019 Aland Sinduartha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
